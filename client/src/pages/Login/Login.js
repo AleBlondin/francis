c7422ffd9ce3248084632282f8623b082e958004
Thibaut Cheymol <thibautc@theodo.fr>
2018-10-21 20:22:31 +0100

    ✨ Move login form logic into service

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -2,39 +2,12 @@
 import { withFormik } from 'formik';
 import InnerLoginForm from './Login.form';
 
-export type FormValues = {
-  email: string,
-  password: string,
-};
-
-export type StateProps = {
-  loginError?: ?string,
-};
-
-export type DispatchProps = {
-  login: (values: FormValues) => Dispatch<LoginUserRequestAction>,
-};
-
-type Props = StateProps & DispatchProps;
+import { validateForm, mapPropsToValues, handleSubmit } from './service';
 
 const LoginFormContainer = withFormik({
-  mapPropsToValues: () =>
-    ({
-      email: '',
-      password: '',
-    }: FormValues),
-  validate: (values: FormValues) => {
-    const errors = {};
-    if (!values.email) {
-      errors.email = 'Email required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-      errors.email = 'Invalid email';
-    }
-    return errors;
-  },
-  handleSubmit: (values: FormValues, { props }: { props: Props }) => {
-    props.login(values);
-  },
+  mapPropsToValues,
+  validate: validateForm,
+  handleSubmit,
 })(InnerLoginForm);
 
 export default LoginFormContainer;
