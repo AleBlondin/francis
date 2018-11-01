0970c97e337b6272a961a5d102fb0303cc885b7a
Hugo Lime <ligohu@outlook.fr>
2018-11-01 18:33:19 +0100

    Replace source-map-explorer with webpack-bundle-analyzer

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,5 +1,16 @@
 const rewireStyledComponents = require('react-app-rewire-styled-components');
+const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer');
 
 module.exports = function override(config, env) {
-  return rewireStyledComponents(config, env);
+  const analyzeBundle = process.argv.indexOf('--analyze-bundle') !== -1;
+  config = rewireStyledComponents(config, env);
+
+  if (analyzeBundle) {
+    config = rewireWebpackBundleAnalyzer(config, env, {
+      analyzerMode: 'static',
+      reportFilename: 'report.html',
+    });
+  }
+
+  return config;
 };
