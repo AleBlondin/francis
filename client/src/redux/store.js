97b8a52331b0744f38dc8e506a2370e4c07832e9
phacks <nicolas.goutay@gmail.com>
2018-11-12 14:47:13 +0100

    Fix: error in connected-react-router crashes the app
    Linked to https://github.com/supasate/connected-react-router/issues/174.
    
    `connected-react-router` needs to be stored under the `router` redux key. App would crash otherwise.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -25,7 +25,7 @@ export default function configureStore(history) {
       : compose;
   /* eslint-enable */
 
-  const rootReducer = createReducer();
+  const rootReducer = createReducer({ router: connectRouter(history) });
   const persistConfig = {
     key: 'root',
     whitelist: ['login'],
