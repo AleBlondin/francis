6ceccb7a5336142c77ef96795faea40a0326f6f0
antkahn <ant.kahn@gmail.com>
2018-11-16 10:59:35 +0100

    Remove useless snapshot test of routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,7 +3,6 @@
 import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router';
 
-// https://m33.gitbook.io/standards/technical-gesture/performance/how-to-build-a-performant-javascript-application/how-to-split-your-code-with-webpack
 const Home = lazy(() => import('./pages/Home'));
 const Login = lazy(() => import('./pages/Login'));
 const Avatar = lazy(() => import('./pages/Avatar'));
