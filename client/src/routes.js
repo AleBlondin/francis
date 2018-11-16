e5ccfa6268f7eca2f73dd91c9c4459c869975b5c
antkahn <ant.kahn@gmail.com>
2018-11-16 10:58:22 +0100

    Split route code with react lazy

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,14 +1,21 @@
 // @flow
-import React from 'react';
+// $FlowFixMe flow is not ready for Suspense or lazy today
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router';
-import { Home, Login, Avatar } from './pages';
+
+// https://m33.gitbook.io/standards/technical-gesture/performance/how-to-build-a-performant-javascript-application/how-to-split-your-code-with-webpack
+const Home = lazy(() => import('./pages/Home'));
+const Login = lazy(() => import('./pages/Login'));
+const Avatar = lazy(() => import('./pages/Avatar'));
 
 const routes = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path="/login" component={Login} />
-    <Route path="/avatar" component={Avatar} />
-  </Switch>
+  <Suspense fallback={<div>Loading...</div>}>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/avatar" component={Avatar} />
+    </Switch>
+  </Suspense>
 );
 
 export default routes;
