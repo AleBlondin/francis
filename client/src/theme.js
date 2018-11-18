f0a19519ec72080bb171ea4063b300d67bc7ef89
Benjamin Grandfond <benjaming@theodo.fr>
2018-11-18 14:59:53 +0100

    Update App snapshot and fix MUI warning

--- client/src/theme.js
+++ client/src/theme.js
@@ -2,6 +2,9 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import amber from '@material-ui/core/colors/amber';
 
 export default createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
   palette: {
     primary: amber,
   },
