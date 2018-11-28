59da1679917680810351dfe4c65077b60bf7a47e
AleBlondin <alexandreblondin@laposte.net>
2018-11-28 12:13:08 +0100

    :bug: fix yarn build in production mdoe

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,11 +1,11 @@
 const rewireStyledComponents = require('react-app-rewire-styled-components');
-const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer');
 
 module.exports = function override(config, env) {
   const analyzeBundle = process.argv.indexOf('--analyze-bundle') !== -1;
   config = rewireStyledComponents(config, env);
 
   if (analyzeBundle) {
+    const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer');
     config = rewireWebpackBundleAnalyzer(config, env, {
       analyzerMode: 'static',
       reportFilename: 'report.html',
